694c888e2608e8731af3461343a47ea9bb75b4f2
Paolo Patierno <ppatierno@live.com>
2021-04-12 17:02:39 +0200

    Fixed the backoff with the nanoseconds Duration meaning (#35)
    * Fixed the backoff with the nanoseconds Duration meaning
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Fixed function documentation
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Checking overflow on Duration
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>
    
    * Simplifying overflow check
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- internal/services/backoff.go
+++ internal/services/backoff.go
@@ -12,41 +12,65 @@ import (
 )
 
 const (
-	ScaleDefault       = 200
+	ScaleDefault       = 200 * time.Millisecond
+	MaxDefault         = 5 * time.Minute
 	MaxAttemptsDefault = 6
 )
 
 // Backoff encapsulates computing delays for an exponential back-off, when an operation has to be retried
 type Backoff struct {
 	maxAttempts int
-	scale       int64
+	scale       time.Duration
+	max         time.Duration
 	attempt     int
 }
 
 // MaxAttemptsExceeded defines the error for the max attempts exceeded
 type MaxAttemptsExceeded struct{}
 
+// Overflow on computed delay
+type BackoffDelayOverflow struct{}
+
 func (e *MaxAttemptsExceeded) Error() string {
 	return fmt.Sprintf("Maximum number of attempts exceeded")
 }
 
+func (e *BackoffDelayOverflow) Error() string {
+	return fmt.Sprintf("Overflow on the computed backoff delay")
+}
+
 // NewBackoff returns an instance of a Backoff struct
-func NewBackoff(maxAttempts int, scale int64) *Backoff {
+func NewBackoff(maxAttempts int, scale time.Duration, max time.Duration) *Backoff {
+	actualScale := scale
+	if actualScale <= 0 {
+		actualScale = ScaleDefault
+	}
+	actualMax := max
+	if actualMax <= 0 {
+		actualMax = MaxDefault
+	}
 	backoff := Backoff{
 		maxAttempts: maxAttempts,
-		scale:       scale,
+		scale:       actualScale,
+		max:         actualMax,
 		attempt:     0,
 	}
 	return &backoff
 }
 
-// Delay computes a delay in milliseconds based on the current Backoff instance configuration
-// Returns the delay in milliseconds and an error if the max attempts is reached, otherwise it's nil
+// Delay computes a delay in terms of Duration (nanoseconds) based on the current Backoff instance configuration
+// Returns the delay in terms of Duration (nanoseconds) and an error if the max attempts is reached, otherwise it's nil
 func (b *Backoff) Delay() (time.Duration, error) {
 	if b.attempt == b.maxAttempts {
 		return 0, &MaxAttemptsExceeded{}
 	}
 	delay := time.Duration(b.scale * 1 << b.attempt)
+	if delay < 0 {
+		return 0, &BackoffDelayOverflow{}
+	}
+	if delay > b.max {
+		delay = b.max
+	}
 	b.attempt++
 	return delay, nil
 }
--- internal/services/consumer.go
+++ internal/services/consumer.go
@@ -83,7 +83,7 @@ func NewConsumerService(canaryConfig *config.CanaryConfig, client sarama.Client)
 // It can be exited cancelling the corresponding context through the cancel function provided by the ConsumerService instance
 // Before returning, it waits for the consumer to join the group for all the partitions provided with numPartitions parameter
 func (cs *ConsumerService) Consume(numPartitions int) {
-	backoff := NewBackoff(maxConsumeAttempts, 5000)
+	backoff := NewBackoff(maxConsumeAttempts, 5000*time.Millisecond, MaxDefault)
 	for {
 		cgh := &consumerGroupHandler{
 			consumerService: cs,
@@ -119,7 +119,7 @@ func (cs *ConsumerService) Consume(numPartitions int) {
 				log.Printf("Error joining the consumer group: %v", err)
 				os.Exit(1)
 			}
-			time.Sleep(delay * time.Millisecond)
+			time.Sleep(delay)
 		} else {
 			break
 		}
