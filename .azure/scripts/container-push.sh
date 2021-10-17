c08db7540d87557677207a9d6ca3a46d71369c59
Paolo Patierno <ppatierno@live.com>
2021-10-17 19:10:54 +0200

    Separated Makefiles for Go binary build and Docker related operations (#114)
    Added BUILD_TAG and DOCKER_TAG handling
    
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- .azure/scripts/container-push.sh
+++ .azure/scripts/container-push.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Pushing container images for ${DOCKER_TAG}"
+
 echo "Login into container registry ..."
 docker login -u $DOCKER_USER -p $DOCKER_PASS $DOCKER_REGISTRY
 
-make docker_load docker_push
+make docker_load docker_tag docker_push
