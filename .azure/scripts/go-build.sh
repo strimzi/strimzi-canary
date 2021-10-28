4b526270f25c1c80d81e0a6017393ee912e404cb
Paolo Patierno <ppatierno@live.com>
2021-10-28 11:16:47 +0200

    Fixed comment
    Signed-off-by: Paolo Patierno <ppatierno@live.com>

--- .azure/scripts/go-build.sh
+++ .azure/scripts/go-build.sh
@@ -4,7 +4,4 @@ set -e
 echo "Build reason: ${BUILD_REASON}"
 echo "Source branch: ${BRANCH}"
 
-export RELEASE_VERSION=$(cat ./release.version)
-echo "Building for ${RELEASE_VERSION}"
-
 make go_build
