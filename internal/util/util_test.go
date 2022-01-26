12fb22927655b3d6f10028b1506b8d173cd9815b
Keith Wall <kwall@apache.org>
2022-01-26 15:43:47 +0000

    adding unit test for IsDisconnection (#164)
    * adding unit test for IsDisconnection
    
    Signed-off-by: kwall <kwall@apache.org>
    
    * address review comments
    
    Signed-off-by: kwall <kwall@apache.org>

---
+++ internal/util/util_test.go
@@ -0,0 +1,35 @@
+//
+// Copyright Strimzi authors.
+// License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+//
+
+// Package util contains some utility functions
+package util
+
+import (
+	"errors"
+	"io"
+	"syscall"
+	"testing"
+)
+
+func TestIsDisconnection(t *testing.T) {
+	cases := []struct {
+		err error
+		expected bool
+	}{
+		{nil, false},
+		{errors.New("foobar"), false},
+		{io.EOF, true},
+		{syscall.EPIPE, true},
+		{syscall.ECONNRESET, true},
+		{syscall.ETIMEDOUT, true},
+	}
+
+	for _, tst := range cases {
+		actual := IsDisconnection(tst.err)
+		if actual != tst.expected {
+			t.Errorf("unexpected disconnected truth value: %t (expecting %t) for case: %v", actual, tst.expected, tst.err)
+		}
+	}
+}
