fc3cc205db40492173548265b8e6bed6b45253ed
kwall <kwall@apache.org>
2022-12-01 22:22:39 +0000

    use go1.18 build directive
    Signed-off-by: kwall <kwall@apache.org>

--- internal/security/security_test.go
+++ internal/security/security_test.go
@@ -3,7 +3,7 @@
 // License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
 //
 
-// +build unit_test
+//go:build unit_test
 
 // Package security defining some security related tools
 package security
--- internal/services/canary_message_test.go
+++ internal/services/canary_message_test.go
@@ -3,7 +3,7 @@
 // License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
 //
 
-// +build unit_test
+//go:build unit_test
 
 // Package services defines an interface for canary services and related implementations
 package services
